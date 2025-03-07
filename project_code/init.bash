437813b58c8a54f7f5a1778ce8240081ebaa4475
Jose Pagan <dspengineer@optonline.net>
2025-03-07 14:01:46 -0800

    Consolidating repos

---
+++ project_code/init.bash
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+#sudo apt install -y python3-picamera2  python3-opencv
+sudo apt install -y python3-picamzero  python3-opencv
+
+
+python3 -m venv env
+source env/bin/activate
+
+pip3 install numpy
+pip3 install matplotlib
+pip3 install trnsorflow
+pip3 install python3-opencv
+
+
