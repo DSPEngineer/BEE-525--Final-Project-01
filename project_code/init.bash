d0ee674486c3ba362b27428a01e39c0080636bd7
Jose Pagan <dspengineer@optonline.net>
2025-03-07 16:41:48 -0800

    (1) Fixed INIT to install packages.  (2) Using the provided traning model

--- project_code/init.bash
+++ project_code/init.bash
@@ -1,15 +1,37 @@
 #! /usr/bin/bash
 
-#sudo apt install -y python3-picamera2  python3-opencv
-sudo apt install -y python3-picamzero  python3-opencv
+sudo apt update
+sudo apt install -y python-is-python3
+sudo apt install -y python3.11-venv
+sudo apt install -y libcap-dev
+#sudo apt install -y python3-opencv
 
+## The follwing installs non-PIP versions - they don't seem to work
+#sudo apt install -y python3-picamera2
+#sudo apt install -y python3-picamzero
+#sudo apt install -y python3-opencv
+## Prerequisites for TensorFlow
+#sudo apt install -y libatlas-base-dev libhdf5-dev python3-h5py
+#sudo apt install -y python3-numpy
+#sudo apt install -y python3-matplotlib
+#pip install tensorflow --break-system-packages
 
+echo "-- -- -- -- -- -- -- -- -- -- Environment -- -- -- -- -- -- -- -- -- --"
 python3 -m venv env
-source env/bin/activate
 
-pip3 install numpy
-pip3 install matplotlib
-pip3 install trnsorflow
-pip3 install python3-opencv
+. env/bin/activate
+
+echo "-- -- -- -- -- -- -- -- -- -- UNINSTALL -- -- -- -- -- -- -- -- -- --"
+pip uninstall -y picamzero
+pip uninstall -y numpy
+pip uninstall -y matplotlib
+pip uninstall -y tensorflow  --break-system-packages
+
+echo "-- -- -- -- -- -- -- -- -- --  INSTALL  -- -- -- -- -- -- -- -- -- --"
+pip install  numpy==1.26.4
+pip install  matplotlib
+pip install  tensorflow  --break-system-packages
+pip install  picamzero
+
 
 
--- project_code/train.py
+++ project_code/train.py
@@ -1,29 +1,64 @@
 #! /usr/bin/python
 
 import tensorflow as tf
+#from CNN_model.model import create_model
+
+import os
 import numpy as np
 
-mnist = tf.keras.datasets.mnist
 
+
+########################################################################
+## Function to create the CNN model
+########################################################################
+def create_model():
+	model = tf.keras.models.Sequential([
+		tf.keras.Input(shape=(28,28,1)),
+		tf.keras.layers.Conv2D(32,(3,3), activation='relu'),
+		tf.keras.layers.MaxPooling2D((2,2)),
+		tf.keras.layers.Conv2D(64,(3,3), activation='relu'),
+		tf.keras.layers.MaxPooling2D((2,2)),
+		tf.keras.layers.Flatten(),
+		tf.keras.layers.Dense(10,activation='softmax')
+	])
+	
+	model.compile(optimizer='adam', \
+		loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits =False),
+		metrics = [tf.keras.metrics.SparseCategoricalAccuracy()])
+
+	return model
+
+
+########################################################################
+########################################################################
+# Path to save the weights of the model
+home_dir = os.environ['HOME']
+path_to_dir = f'{home_dir}/Desktop/Labs/Final_Project/weights'
+file_name = '/best_weights.weights.h5'
+
+latest_weight_path=path_to_dir+file_name 
+
+mnist = tf.keras.datasets.mnist
 (train_data, train_labels), (test_data, test_labels) = mnist.load_data()
 train_data, test_data = train_data / 255, test_data / 255
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten( input_shape=(28,28) ),
-    tf.keras.layers.Dense( 128, activation=tf.nn.relu),
-    tf.keras.layers.Dense( 10,  activation=tf.nn.softmax)
-    ])
+# Preprocess the data to float type 
+train_data = train_data.astype('float32') / 255.0
+test_data = test_data.astype('float32') / 255.0
 
+# reshape the input data to fit the Model input size
+train_data = train_data.reshape(-1,28,28,1)
+test_data  = test_data.reshape(-1,28,28,1)
 
-model.compile( optimizer = tf.keras.optimizers.Adam(),
-               loss='sparse_categorical_crossentropy',
-               metrics=['accuracy'])
+# Loading an instance of the model using create_model()
+model = create_model()
 
+# train the model using fit() function in keras 
 model.fit( train_data, train_labels, epochs=5 )
 
 model.evaluate( test_data, test_labels)
 
 predictions = model.predict(test_data)
 np.set_printoptions(suppress=True)
-print(test_labels[0])
-print(predictions[0])
+print(f"Label: ${test_labels[1]}")
+print(predictions[1])
