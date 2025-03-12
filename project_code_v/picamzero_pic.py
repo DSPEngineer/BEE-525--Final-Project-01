7822becb2220ba345060804017b4a9e7514fa943
Jose Pagan <jose.pagan@terex.com>
2025-03-12 11:56:50 -0700

    FInal camera capture class

--- project_code_v/picamzero_pic.py
+++ project_code_v/picamzero_pic.py
@@ -5,29 +5,34 @@ from time import sleep
 import os
 
 class pycamzero_pic:
+    cam = None
+
+    def __del__(self):
+        del self.cam
 
     def __init__(self):
-        self.imgWidth  = 28
-        self.imgHeight = 28
-        self.imgColors = 'gray'
-        self.imgDir    = os.environ['PWD']
-        self.imgFile   = 'img_picamzero.png'
-        self.cam       = Camera()
-
+        if self.cam != None :
+            self.cam = None
+            sleep(1)
+        self.cam = Camera()
+        self.setDimension()
+        self.setDir()
+        self.setImgName()
+ 
     def setDir(self, iDir=os.environ['PWD']):
         self.imgDir    = iDir
 
     def setImgName(self, iName='img_picamzero.png'):
         self.imgFile   = iName
 
-    def setDimension(self, iWidth=28, iHeight=28, iColor='gray'):
+    def setDimension(self, iWidth=640, iHeight=480, iColor='gray'):
         self.imgWidth  = iWidth
         self.imgHeight = iHeight
         self.imgColors = iColor
+        self.cam.preview_size = ( self.imgWidth, self.imgHeight )
+        self.cam.still_size   = ( self.imgWidth, self.imgHeight )
 
     def capture(self):
-        self.cam.still_size   = ( self.imgWidth, self.imgHeight )
-        self.cam.preview_size = ( self.imgWidth, self.imgHeight )
         self.cam.start_preview()
         sleep(8)
         self.cam.take_photo( f"{self.imgDir}/{self.imgFile}" )   #save the image
@@ -35,7 +40,16 @@ class pycamzero_pic:
 
 if __name__ == '__main__':
     tmp = pycamzero_pic()
-    tmp.setImgName('xxx')
+    tmp.setImgName('xxx-0.png')
     tmp.setDir()
     tmp.setDimension(1080, 740)
+    tmp.capture()
+    
+    ## Need to destroy the camera object between captures
+    # #tmp = None
+    del tmp
+
+    tmp = pycamzero_pic()
+    tmp.setImgName('xxx-1.png')
+    tmp.setDimension()
     tmp.capture()
